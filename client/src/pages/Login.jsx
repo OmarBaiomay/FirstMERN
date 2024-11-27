c633edfd03963189d9a5fe2789c5d0024f0a0597
Omar Al-Bayomi <baiomayomar@gmail.com>
2024-11-28 00:46:47 +0200

    first commit

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import API from '../utils/api';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const { data } = await API.post('/auth/login', { email, password });
+      console.log('Logged in:', data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
