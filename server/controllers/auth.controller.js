a272a8d05730e28a2df263c4234e77285e1533c5
Omar Al-Bayomi <baiomayomar@gmail.com>
2024-12-04 00:40:12 +0200

    ADD [Login - Logout- update profile] - Backend

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -71,10 +72,67 @@ export const signup = async (req, res) => {
 };
 
 
-export const login = (req, res) =>{
-    res.send("Login")
+export const login = async (req, res) =>{
+    const {email, password} = req.body;
+    
+    try {
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.status(400).json({message: "Invalid Credentials"})
+        }
+        
+        const isPassCorrect = await bcrypt.compare(password, user.password)
+
+        if(!isPassCorrect){
+            return res.status(400).json({message: "Invalid Credentials"})
+        }
+
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            profilePic: user.profilePic
+        })
+
+    } catch (error) {
+        console.log("Error in login Controller", error.message)
+    }
 };
 
 export const logout = (req, res) =>{
-    res.send("Log Out")
+    try {
+        res.cookie("jwt_token", "", {maxAge:0})
+        res.status(200).json({message: "Logged out Successfully"})
+    } catch (error) {
+        console.log("Error in logout Controller", error.message)
+    }
+};
+
+export const updateProfile = async (req, res) =>{
+    try {
+        const {profilePic} = req.body
+
+        const userId =  req.user._id
+
+        if(!profilePic){
+            return res.status(400).json({message: "Profile pic is required"});
+        }
+
+        const uploadRes = await cloudinary.uploader.upload(profilePic)
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            profilePic: uploadRes.secure_url
+        }, {new: true})
+
+        return res.status(200).json(updatedUser);
+        
+    } catch (error) {
+        console.log("Error in Update Profile Controller", error.message)
+        return res.status(500).json({message: "Internal Server Error"});
+    }
 };
--- server/server.js
+++ server/server.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+import express, { json } from 'express';
+import { connect } from 'mongoose';
+import { config } from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-dotenv.config();
+config();
 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(json());
+app.use(cookieParser());
 
 // Routes
 // app.use('/api/auth', require('./routes/authRoutes'));
 
 // MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
 
